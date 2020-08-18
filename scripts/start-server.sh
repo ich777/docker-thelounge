9610840085e3d1c3d4072bc2ccd8a2a275edd359
ich777 <ich777@noreply.example.org>
2020-08-18 15:50:23 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+export THELOUNGE_HOME="${DATA_DIR}"
 
-sleep infinity
+echo "---Prepare Server---"
+USERS="$(ls ${DATA_DIR}/users | sed 's/\.json//g')"
+
+if grep -q "$USERNAME" <<< "$USERS"; then
+		echo "---User: $USERNAME found! Nothing to do, continuing!---"
+	else
+		echo "---Creating user: $USERNAME, Save to Log: $SAVELOG---"
+		echo -e "$USERPASSWORD\n$SAVELOG\n" | thelounge add $USERNAME
+fi
+
+chmod -R ${DATA_PERM} ${DATA_DIR}
+echo "---Server ready---"
+
+echo "---Starting TheLounge---"
+cd ${DATA_DIR}
+thelounge start
