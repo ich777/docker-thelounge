5f15b4d6eab6d15504e84d9da7ab3d6b40e518df
ich777 <ich777@noreply.example.org>
2020-08-18 17:14:56 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -5,10 +5,11 @@ echo "---Prepare Server---"
 USERS="$(ls ${DATA_DIR}/users | sed 's/\.json//g')"
 
 if grep -q "$USERNAME" <<< "$USERS"; then
-		echo "---User: $USERNAME found! Nothing to do, continuing!---"
-	else
-		echo "---Creating user: $USERNAME, Save to Log: $SAVELOG---"
-		echo -e "$USERPASSWORD\n$SAVELOG\n" | thelounge add $USERNAME
+	echo "---User: $USERNAME found! Nothing to do, continuing!---"
+else
+	echo "---Creating user: $USERNAME, Save to Log: $SAVELOG---"
+	screen -d -m /opt/scripts/create-user.sh
+	sleep 4
 fi
 
 chmod -R ${DATA_PERM} ${DATA_DIR}
